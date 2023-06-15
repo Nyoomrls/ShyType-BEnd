f25b45bb7124fddc598c506dd8a47c0315b5bb42
Nyoomrls <neomorales24.nm@gmail.com>
2023-06-15 23:50:41 +0800

    Blocker User FUNction
    The function now works, blockerID and blockedID is now inserted into the database table, change table name from "Block" to Blocks"

--- app/Http/Controllers/BlockController.php
+++ app/Http/Controllers/BlockController.php
@@ -4,17 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Block;
+use Termwind\Components\Dd;
 
 class BlockController extends Controller
 {
     //
     public function blockUser(Request $request)
     {
-        // $block = Block::create([
-        //     "blockerID" => $request->blockerID,
-        //     "blockedID" => $request->blockedID,
-        // ]);
-        // $block->save();
+
+        $block = Block::create([
+            "blockerID" => $request->blockerID,
+            "blockedID" => $request->blockedID,
+        ]);
+        $block->save();
+        return [
+            "blockerID" => $request->blockerID,
+            "blockedID" => $request->blockedID,
+            "message" => "Napupunta ka sa Back-End pero may prob sa database",
+            "status" => 200,
+            // "data" => $block,
+            // "data" => blocked,    
+        ];
 
         // $BlockerID = Block::where('blockerID', $blockerID)->get();
         // $fields = Validator::make($request->all(), [
@@ -22,22 +32,16 @@ class BlockController extends Controller
         //     'blockedID' => ['required', 'integer'],
         // ]);
 
-        // $block = Block::create([
-        //     "blockerID" => $request->blockerID['blockerId'],
-        //     "blockedID" => $request->blockedID['blockedId'],
+        // $block = Block::insert([
+        //     "blockerID" => $request->blockerID['blockerID'],
+        //     "blockedID" => $request->blockedID['blockedID'],
         // ]);
-        // $Newblock = $block->save();
+        // $block->save();
 
         // return [
         //     "message" => "TesT",
         //     "data" => $Newblock,
         //     "status" => 300,
         // ];
-        return[
-            "message" => "dito na ikaw bous",
-            // "data" => $blockedID,
-            // "data" => blocked,    
-            "status" => 300,
-        ];
     }
 }
--- database/migrations/2023_06_07_100445_create_block_table.php
+++ database/migrations/2023_06_07_100445_create_block_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('block', function (Blueprint $table) {
+        Schema::create('blocks', function (Blueprint $table) {
             $table->id();
             $table->integer("blockerID");
             $table->integer("blockedID");
