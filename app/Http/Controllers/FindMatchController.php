591d9635bad9d5db85139bdc9be534992ff1397e
Nyoomrls <neomorales24.nm@gmail.com>
2023-06-22 23:11:47 +0800

    Blocked Already Matched Filter
    Excluded in the pool of matches user that are block and is already matched with current user

--- app/Http/Controllers/FindMatchController.php
+++ app/Http/Controllers/FindMatchController.php
@@ -16,25 +16,36 @@ class FindMatchController extends Controller
         $personality = User::find($request->userId)->Personality;
         // $user = Personality::WHERE('user_id', $request->userId)->first();
 
+
         $matches = DB::table('personalities')
             ->select(
                 DB::raw("personalities.user_id, users.*,
-                    CASE WHEN personalities.question1 = $personality->question1 THEN 1 ELSE 0 END +
-                    CASE WHEN personalities.question2 = $personality->question2 THEN 1 ELSE 0 END +
-                    CASE WHEN personalities.question3 = $personality->question3 THEN 1 ELSE 0 END +
-                    CASE WHEN personalities.question4 = $personality->question4 THEN 1 ELSE 0 END +
-                    CASE WHEN personalities.question5 = $personality->question5 THEN 1 ELSE 0 END +
-                    CASE WHEN personalities.question6 = $personality->question6 THEN 1 ELSE 0 END +
-                    CASE WHEN personalities.question7 = $personality->question7 THEN 1 ELSE 0 END +
-                    CASE WHEN personalities.question8 = $personality->question8 THEN 1 ELSE 0 END +
-                    CASE WHEN personalities.question9 = $personality->question9 THEN 1 ELSE 0 END +
-                    CASE WHEN personalities.question10 = $personality->question10 THEN 1 ELSE 0 END AS score
-                    "),
+                CASE WHEN personalities.question1 = $personality->question1 THEN 1 ELSE 0 END +
+                CASE WHEN personalities.question2 = $personality->question2 THEN 1 ELSE 0 END +
+                CASE WHEN personalities.question3 = $personality->question3 THEN 1 ELSE 0 END +
+                CASE WHEN personalities.question4 = $personality->question4 THEN 1 ELSE 0 END +
+                CASE WHEN personalities.question5 = $personality->question5 THEN 1 ELSE 0 END +
+                CASE WHEN personalities.question6 = $personality->question6 THEN 1 ELSE 0 END +
+                CASE WHEN personalities.question7 = $personality->question7 THEN 1 ELSE 0 END +
+                CASE WHEN personalities.question8 = $personality->question8 THEN 1 ELSE 0 END +
+                CASE WHEN personalities.question9 = $personality->question9 THEN 1 ELSE 0 END +
+                CASE WHEN personalities.question10 = $personality->question10 THEN 1 ELSE 0 END AS score
+            "),
             )
             ->leftJoin("users", "users.id", "=", "personalities.user_id")
+            ->leftJoin("blocks", function ($join) use ($request) {
+                $join->on("users.id", "=", "blocks.blockedID")
+                    ->where("blocks.blockerID", "=", $request->userId);
+            })
+            ->leftJoin("matches", function ($join) use ($request) {
+                $join->on("users.id", "=", "matches.matchUser")
+                    ->where("matches.userID", "=", $request->userId);
+            })
             ->where("personalities.user_id", "<>", $request->userId)
             ->where("users.gender", "=", $user->matchgender)
-            ->having("score", ">=", 6) //Add the score filter
+            ->whereNull("blocks.blockedID") // Exclude blocked IDs
+            ->whereNull("matches.matchUser") // Exclude matches from "matches" table
+            ->having("score", ">=", 6) // Add the score filter
             ->get();
 
         return [
@@ -42,6 +53,33 @@ class FindMatchController extends Controller
             "data" => $matches,
             "status" => 200
         ];
+
+        // $matches = DB::table('personalities')
+        //     ->select(
+        //         DB::raw("personalities.user_id, users.*,
+        //             CASE WHEN personalities.question1 = $personality->question1 THEN 1 ELSE 0 END +
+        //             CASE WHEN personalities.question2 = $personality->question2 THEN 1 ELSE 0 END +
+        //             CASE WHEN personalities.question3 = $personality->question3 THEN 1 ELSE 0 END +
+        //             CASE WHEN personalities.question4 = $personality->question4 THEN 1 ELSE 0 END +
+        //             CASE WHEN personalities.question5 = $personality->question5 THEN 1 ELSE 0 END +
+        //             CASE WHEN personalities.question6 = $personality->question6 THEN 1 ELSE 0 END +
+        //             CASE WHEN personalities.question7 = $personality->question7 THEN 1 ELSE 0 END +
+        //             CASE WHEN personalities.question8 = $personality->question8 THEN 1 ELSE 0 END +
+        //             CASE WHEN personalities.question9 = $personality->question9 THEN 1 ELSE 0 END +
+        //             CASE WHEN personalities.question10 = $personality->question10 THEN 1 ELSE 0 END AS score
+        //             "),
+        //     )
+        //     ->leftJoin("users", "users.id", "=", "personalities.user_id")
+        //     ->where("personalities.user_id", "<>", $request->userId)
+        //     ->where("users.gender", "=", $user->matchgender)
+        //     ->having("score", ">=", 6) //Add the score filter
+        //     ->get();
+
+        // return [
+        //     "message" => "Successfully pulled data",
+        //     "data" => $matches,
+        //     "status" => 200
+        // ];
     }
 
     public function recent_match(Request $request)
